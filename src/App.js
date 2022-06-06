da2f326258d08dcdb589973e22256cd82dfaae97
praveenkumar-ctrl <70391240+praveenkumar-ctrl@users.noreply.github.com>
2022-06-06 08:48:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,9 +28,9 @@ function App() {
   const [distance, setDistance] = useState('')
   const [duration, setDuration] = useState('')
 
-  /** @type React.MutableRefObject<HTMLInputElement> */
+ 
   const originRef = useRef()
-  /** @type React.MutableRefObject<HTMLInputElement> */
+  
   const destiantionRef = useRef()
 
   if (!isLoaded) {
@@ -67,84 +67,7 @@ function App() {
     <>
    
     
-    {/* <Flex
-      position='relative'
-      flexDirection='column'
-      alignItems='center'
-      h='100vh'
-      w='100vw'
-    > */}
-      {/* <Box position='absolute' left={100} top={30} h='100%' w='100%'>
-       
-        <GoogleMap
-          center={center}
-          zoom={15}
-          mapContainerStyle={{ width: '50%', height: '50%' }}
-          options={{
-            zoomControl: false,
-            streetViewControl: false,
-            mapTypeControl: false,
-            fullscreenControl: false,
-          }}
-          onLoad={map => setMap(map)}
-        >
-          <Marker position={center} />
-          {directionsResponse && (
-            <DirectionsRenderer directions={directionsResponse} />
-          )}
-        </GoogleMap>
-      </Box> */}
-      {/* <Box
-        p={4}
-        borderRadius='lg'
-        m={4}
-        bgColor='white'
-        shadow='base'
-        minW='container.md'
-        zIndex='1'
-      >
-        <HStack spacing={2} justifyContent='center'>
-          <Box flexGrow={1}>
-            <Autocomplete>
-              <Input type='text' placeholder='Origin' ref={originRef} />
-            </Autocomplete>
-          </Box>
-          <Box flexGrow={1}>
-            <Autocomplete>
-              <Input
-                type='text'
-                placeholder='Destination'
-                ref={destiantionRef}
-              />
-            </Autocomplete>
-          </Box>
-
-          <ButtonGroup>
-            <Button colorScheme='pink' type='submit' onClick={calculateRoute}>
-              Calculate Route
-            </Button>
-            <IconButton
-              aria-label='center back'
-              icon={<FaTimes />}
-              onClick={clearRoute}
-            />
-          </ButtonGroup>
-        </HStack>
-        <HStack spacing={4} mt={4} justifyContent='space-between'>
-          <Text>Distance: {distance} </Text>
-          <Text>Duration: {duration} </Text>
-          <IconButton
-            aria-label='center back'
-            icon={<FaLocationArrow />}
-            isRound
-            onClick={() => {
-              map.panTo(center)
-              map.setZoom(15)
-             }}
-          />
-        </HStack>
-      </Box>
-    </Flex> */}
+   
      <div class="nav1">
             <nav class="navbar navbar-light bg-light ">
                 <a class="navbar-brand" href="/">
